01b86e6d52b0445a2bb48aa0c96e998f27081e7c
MargheritaMottana <margherita.mottana@gmail.com>
2024-09-21 20:00:11 +0200

    creazione terza sezione del main lato grafica, creati i data e popolata dinamicamente tutta la sezione

--- src/components/PageMain_3.vue
+++ src/components/PageMain_3.vue
@@ -2,7 +2,34 @@
   export default {
 	    data() {
 	     return { 
-			title: 'Trusted by Leading Organisations'
+			title: 'Trusted by Leading Organisations',
+            cards:[
+                {
+                    img: '/img/leading_organisations/case-studies-1.jpg',
+                    alt: 'Spaces',
+                    url: '#',
+                    text: 'How SPaces attracted five millions visitors by improving the content',
+                    data: '200%',
+                    description: 'Higher revenue from digital',
+                },
+                {
+                    img: '/img/leading_organisations/case-studies-4.jpg',
+                    alt: 'Hemisferio',
+                    url: '#',
+                    text: 'Creativity helped Hemisferio to increase their brand reach vertically',
+                    data: '10x',
+                    description: 'Sales increase with the same ad spend',
+                },
+                {
+                    img: '/img/leading_organisations/case-studies-6.jpg',
+                    alt: 'DigitalBox',
+                    url: '#',
+                    text: 'How DigitalBox used AI-powered data insight to boost sales',
+                    data: '3-year',
+                    description: 'Partnership with Avada Consultant',
+                }
+            ],
+            button: 'Read More Case Studies'
 	      }
 	   },
 	 }
@@ -13,13 +40,37 @@
     <div class="container">
         <!-- titolo e descizione -->
         <div class="my-container text-center">
-
-            <h2 class="fw-bold pb-3">
+            <h2 class="fw-bold pb-5">
                 {{ title }}
             </h2>
-            <p>
-                {{ text }}
-            </p>
+
+            <!-- cards -->
+             <div class="row mb-5">
+                <div class="col" v-for="(card, i) in cards" :key="i">
+                    <!-- immagine -->
+                    <div class="case-studies mb-3 img-container">
+                        <img :src="card.img" :alt="card.alt" class="case-studies">
+                        <a :href="card.url" class="gradient"></a>
+                    </div>
+                    <!-- testi -->
+                    <div class="case-studies text-start">
+                        <p class="fs-5 mb-5">
+                            {{ card.text }}
+                        </p>
+                        <h3 class="fw-bold">
+                            {{ card.data }}
+                        </h3>
+                        <div class="font-small">
+                            {{ card.description }}
+                        </div>
+                    </div>
+                </div>
+             </div>
+
+             <!-- bottone -->
+              <button class="button orange-button mb-5">
+                {{ button }}
+              </button>
         </div>
     </div>
 
